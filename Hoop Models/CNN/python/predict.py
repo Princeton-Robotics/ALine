463694c9d84a610fc2c9fd8c435e48f38783da9d
Sacheth S <sacheth.sathyanarayanan@outlook.com>
2019-02-16 16:43:22 -0500

    Added CNN for Hoop Detector
    And also made space for HOG

---
+++ Hoop Models/CNN/python/predict.py
@@ -0,0 +1,48 @@
+import sys
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from object_detection import ObjectDetection
+
+MODEL_FILENAME = 'model.pb'
+LABELS_FILENAME = 'labels.txt'
+
+class TFObjectDetection(ObjectDetection):
+    """Object Detection class for TensorFlow
+    """
+    def __init__(self, graph_def, labels):
+        super(TFObjectDetection, self).__init__(labels)
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            tf.import_graph_def(graph_def, name='')
+            
+    def predict(self, preprocessed_image):
+        inputs = np.array(preprocessed_image, dtype=np.float)[:,:,(2,1,0)] # RGB -> BGR
+
+        with tf.Session(graph=self.graph) as sess:
+            output_tensor = sess.graph.get_tensor_by_name('model_outputs:0')
+            outputs = sess.run(output_tensor, {'Placeholder:0': inputs[np.newaxis,...]})
+            return outputs[0]
+
+
+def main(image_filename):
+    # Load a TensorFlow model
+    graph_def = tf.GraphDef()
+    with tf.gfile.FastGFile(MODEL_FILENAME, 'rb') as f:
+        graph_def.ParseFromString(f.read())
+
+    # Load labels
+    with open(LABELS_FILENAME, 'r') as f:
+        labels = [l.strip() for l in f.readlines()]
+
+    od_model = TFObjectDetection(graph_def, labels)
+
+    image = Image.open(image_filename)
+    predictions = od_model.predict_image(image)
+    print(predictions)
+    
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print('USAGE: {} image_filename'.format(sys.argv[0]))
+    else:
+        main(sys.argv[1])
