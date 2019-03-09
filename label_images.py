136d29de017cecf1c13a9a8712d8bb19b5649ed5
kyleaj <35408177+kyleaj@users.noreply.github.com>
2019-03-09 16:14:09 -0500

    Update label_images.py
    Support multiple images and accidental multiclicks

--- label_images.py
+++ label_images.py
@@ -10,6 +10,7 @@ image_directory = sys.argv[1]
 curr_img = None
 
 points = []
+pointsList = []
 
 def click_and_crop(event, x, y, flags, param):
 	# if the left mouse button was clicked, record the starting
@@ -44,9 +45,21 @@ def click_and_crop(event, x, y, flags, param):
 		else:
 			print('Already drew 4 points!')
 
+def drawCurrPoints(curr_img, pointsList):
+	for points in pointsList:
+		cv2.circle(curr_img, points[0], 10, (0, 255, 255))
+		cv2.circle(curr_img, points[1], 10, (0, 255, 255))
+		cv2.circle(curr_img, points[2], 10, (0, 255, 255))
+		cv2.circle(curr_img, points[3], 10, (0, 255, 255))
+		cv2.line(curr_img, points[0], points[1], (0,0,255), 2)
+		cv2.line(curr_img, points[0], points[2], (0,0,255), 2)
+		cv2.line(curr_img, points[1], points[3], (0,0,255), 2)
+		cv2.line(curr_img, points[2], points[3], (0,0,255), 2)
+
 def main():
 	global curr_img
 	global points
+	global pointsList
 	cv2.namedWindow("image")
 	cv2.setMouseCallback("image", click_and_crop)
 	filenames = os.listdir(image_directory)
@@ -59,12 +72,13 @@ def main():
 			print(filename)
 			curr_img_copy = curr_img.copy()
 			curr_img = imutils.resize(curr_img, width = 500)
+			drawCurrPoints(curr_img, pointsList)
 			cv2.imshow('image', curr_img)
 
 			key = cv2.waitKey(0)
 
 			if (key == ord("r")):
-				print('Resetting image')
+				print('Resetting image, including all already labeled hoops')
 				points = []
 				curr_img = curr_img_copy
 				curr_img = imutils.resize(curr_img, width = 500)
@@ -72,20 +86,34 @@ def main():
 				print('Saving Points')
 				# Saving points
 				j = 0
-				if (not(len(points) == 4)):
+				if (not((len(points) % 4) == 0)):
 					print('Invalid number of points')
 					curr_img = curr_img_copy
 					curr_img = imutils.resize(curr_img, width = 500)
 				else:
+					pointsList.append(points)
 					with open(filename + '.txt', "w+") as out_file:
 						(oldwidth, oldh, oldc) = curr_img.shape
 						(newwidth, newh, newc) = curr_img_copy.shape
 						ratio = (newwidth / oldwidth)
-						while (j < 4):
-							points[j] = tuple(int(ratio * x) for x in points[j])
-							out_file.write(str(points[j]))
-							j = j + 1
+						for thesePoints in pointsList:
+							j = 0
+							while (j < 4):
+								thesePoints[j] = tuple(int(ratio * x) for x in points[j])
+								out_file.write(str(thesePoints[j]))
+								j = j + 1
+							out_file.write('\n')
 					i = i + 1
+					pointsList = []
+				points = []
+			elif (key == ord("n")):
+				if (not((len(points) % 4) == 0)):
+					print('Invalid number of points')
+					curr_img = curr_img_copy
+					curr_img = imutils.resize(curr_img, width = 500)
+				else:
+					pointsList.append(points)
+					print('You can now label another hoop')
 				points = []
 			else:
 				print('Invalid key press')
